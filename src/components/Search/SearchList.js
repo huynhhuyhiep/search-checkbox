a34275ddacce6389da52cb21634fd2df6ae6d848
Hiep Huynh <huynhhuyhiep0808@gmail.com>
2020-10-01 10:26:53 +0700

    Update UI Search component

--- src/components/Search/SearchList.js
+++ src/components/Search/SearchList.js
@@ -2,38 +2,40 @@ import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 
 const SearchList = () => {
-  const { data, loading, error } = useSelector((state) => state);
-
-  const renderLoading = () => {
-    if (!loading || data.length > 0) return null;
-    return <div className="news-loading">Loading...</div>;
-  };
+  const { data, loading, error } = useSelector(state => state);
 
   const renderEmpty = () => {
     if (loading || error || data.length > 0) return null;
-    return <div className="news-empty">Not Found</div>;
+    return <div className="news-empty flex-center">Not Found</div>;
   };
 
   const renderError = () => {
     if (!error || loading) return null;
-    return <div className="news-error">{error}</div>;
+    return <div className="news-error flex-center">{error}</div>;
   };
 
-  // prevent show Not found when component init
+  // prevent show Not found when mounting component
   if (loading === null) return null;
   return (
     <div className="news-container">
-      {renderLoading()}
-      {data?.map((item) => {
-        const { _highlightResult, objectID } = item;
-        const { title, story_text } = _highlightResult || {};
+      {data?.map(item => {
+        const { _highlightResult, objectID, url } = item;
+        const { title, story_text, url: urlHighlight } = _highlightResult || {};
         return (
           <div key={objectID} className="news-item">
             {title && (
-              <div
-                className="news-item-title"
-                dangerouslySetInnerHTML={{ __html: title.value }}
-              />
+              <a href={url} target="_blank">
+                {urlHighlight && (
+                  <div
+                    className="news-item-url"
+                    dangerouslySetInnerHTML={{ __html: urlHighlight.value }}
+                  />
+                )}
+                <div
+                  className="news-item-title"
+                  dangerouslySetInnerHTML={{ __html: title.value }}
+                />
+              </a>
             )}
             {story_text && (
               <div
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,26 +1,36 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useState } from 'react';
 import SearchList from './SearchList';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchNews } from './reducer';
 import './Search.css';
+import { useDebounce } from '../../hook';
+
+const Loader = memo(() => {
+  const loading = useSelector(state => state.loading);
+  if (!loading) return null;
+  return <div className="loader" />;
+});
 
 const Search = () => {
   const dispatch = useDispatch();
-  const handleChange = useCallback(
-    e => {
-      dispatch(fetchNews(e.target.value));
-    },
-    [dispatch]
-  );
+  const [value, setValue] = useState('');
+
+  useDebounce(() => dispatch(fetchNews(value)), 100, [value]);
+  const handleChange = useCallback(e => {
+    setValue(e.target.value);
+  }, []);
 
   return (
     <>
-      <div className="search-input-container flex-row align-center">
-        <input
-          onChange={handleChange}
-          placeholder="Search…"
-          className="search-input"
-        />
+      <div className="search-header flex-row align-center">
+        <div className="search-input-container flex-row">
+          <input
+            onChange={handleChange}
+            placeholder="Search…"
+            className="search-input"
+          />
+          <Loader />
+        </div>
       </div>
       <SearchList />
     </>
